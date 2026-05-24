543d758b2a5f6d01ab039b519552641b79eb5802
Aiden <aiden@vigue.me>
2026-05-24 12:43:53 -0400

    fix

--- ios/App/ci_scripts/ci_post_clone.sh
+++ ios/App/ci_scripts/ci_post_clone.sh
@@ -4,6 +4,9 @@ set -e
 # Xcode Cloud-provided path to the cloned repo.
 REPO_ROOT="$CI_PRIMARY_REPOSITORY_PATH"
 
+echo "Initializing git submodules..."
+git -C "$CI_PRIMARY_REPOSITORY_PATH" submodule update --init --recursive
+
 echo "Installing Node.js..."
 brew install node
 
